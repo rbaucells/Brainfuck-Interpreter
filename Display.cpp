3b10d4b39c93abd90aa636d0ee5af14dbd3fce46
Ricardo <ricardo@baucells.net>
2025-08-17 22:38:01 -0400

    Tried and failed to add libraries

---
+++ Display.cpp
@@ -0,0 +1,45 @@
+// #include <string>
+// #include "common.h"
+// #include "glad/gl.h"
+// #include "GLFW/glfw3.h"
+//
+// Display::Display(const std::string& bfProgram) {
+//     this -> bfProgram = bfProgram;
+//
+//     // glfwSetErrorCallback(error_callback);
+//
+//     if (!glfwInit())
+//     {
+//         std::printf("glfw initialization failed");
+//         exit(0);
+//     }
+//
+//     GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+//     if (!window)
+//     {
+//         std::printf("window creation failed");
+//         exit(0);
+//     }
+//
+//     glfwMakeContextCurrent(window);
+//     gladLoadGL(glfwGetProcAddress);
+// }
+//
+// void error_callback(int error, const char* description)
+// {
+//     std::printf("Error with code'%d': %s\n", error, description);
+// }
+//
+// void Display::update() {
+//
+// }
+//
+// Display::~Display() {
+//     glfwTerminate();
+// }
+//
+//
+//
+//
+//
+//
---
+++ main.cpp
@@ -0,0 +1,5 @@
+// #include "GLFW/glfw3.h"
+//
+// int main() {
+//     glfwInit();
+// }
